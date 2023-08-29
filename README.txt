5c1d272ff599f3f43ef5e6148971386dcdba23f9
Steve Jones <steve@stevejonesdev.com>
2023-08-29 16:42:42 -0400

    Added - support for FacetWP dynamic content

--- README.txt
+++ README.txt
@@ -3,7 +3,7 @@ Contributors: equalizedigital, alh0319, stevejonesdev
 Tags: accessibility, accessible, wcag, ada, a11y, section 508, links, open new window, open new tab
 Requires at least: 5.0.0
 Tested up to: 6.3.1
-Stable tag: 1.0.7
+Stable tag: 1.0.8
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -73,6 +73,9 @@ The Accessibility New Window Warnings plugin is ready for localization. We have 
 If you would like to contribute a translation to this plugin, please [contact us](https://equalizedigital.com/contact/) or [fork the repository on GitHub](https://github.com/equalizedigital/accessibility-new-window-warnings).
 
 == Changelog ==
+
+= 1.0.8 =
+* Added - support for FacetWP dynamic content
 
 = 1.0.7 =
 * Updated - textdomain to plugin slug
--- assets/js/accessibility-new-window-warnings.js
+++ assets/js/accessibility-new-window-warnings.js
@@ -1,86 +1,92 @@
 (function ($) {
   "use strict";
 
-  $(window).on('load',function () {
+  var anww_link_tooltip;
 
-    var anww_label = '';
-    
-    /**
-     * Accessible _blank link tooltip
-     */
-    var anww_link_tooltip = $('<div/>').css({
-      position: 'absolute',
-      background: 'white',
-      color: '#1e1e1e',
-      fontSize: '16px',
-      border: '1px solid black',
-      padding: '5px 10px',
-      zIndex: 9999,
-      display: 'none'
-    }).addClass('anww-tooltip').appendTo('body');
+  function initializeTooltip() {
+      anww_link_tooltip = $('<div/>').css({
+          position: 'absolute',
+          background: 'white',
+          color: '#1e1e1e',
+          fontSize: '16px',
+          border: '1px solid black',
+          padding: '5px 10px',
+          zIndex: 9999,
+          display: 'none'
+      }).addClass('anww-tooltip').appendTo('body');
+  }
 
-    /**
-     * loop through each link with a target of _blank
-     */
-    $("a[target=_blank]").each(function(){
+  function processLinks() {
+      var anww_label = '';
 
-      // add icon to link
-      if($(':header',this).length){
-        $(':header',this).append('<i class="anww-external-link-icon" aria-hidden="true"></i>');
-      }else{
-        $(this).append('<i class="anww-external-link-icon" aria-hidden="true"></i>');
-      }
-      
-      // get aria label text
-      if($(this).attr("aria-label")){
-        anww_label = $(this).attr("aria-label");
-      }else if($('img', $(this)).length){
-        anww_label = $(this).find("img").attr("alt");
-      }else if($(this).text()){
-        anww_label = $(this).text();
-      }
+      // Remove previously appended icons to avoid duplication
+      $(".anww-external-link-icon").remove();
 
-      // add warning label
-      if(anww_label){
-        anww_label = anww_label.trimEnd();
-        anww_label += ", " + anww_localized.opens_a_new_window;
-      }else{
-        anww_label += anww_localized.opens_a_new_window;
-      }
+      $("a[target=_blank]").each(function () {
+          // Add icon to link
+          if ($(':header', this).length) {
+              $(':header', this).append('<i class="anww-external-link-icon" aria-hidden="true"></i>');
+          } else {
+              $(this).append('<i class="anww-external-link-icon" aria-hidden="true"></i>');
+          }
 
-      // add aria-label to link 
-      $(this).attr("aria-label", anww_label);
+          // Get aria label text
+          if ($(this).attr("aria-label")) {
+              anww_label = $(this).attr("aria-label");
+          } else if ($('img', $(this)).length) {
+              anww_label = $(this).find("img").attr("alt");
+          } else if ($(this).text()) {
+              anww_label = $(this).text();
+          }
 
-      // position and show link_tooltip on hover
-      $(this).mousemove(function(e){
-        anww_link_tooltip.css({
-          top: e.pageY + 10 + 'px',
-          left: e.pageX + 10 + 'px'
-        });
-      })
-      .hover(function(){
-        anww_link_tooltip.show().html(anww_localized.opens_a_new_window);
-      }, function(){
-        anww_link_tooltip.hide();
+          // Add warning label
+          if (anww_label) {
+              anww_label = anww_label.trimEnd();
+              anww_label += ", " + anww_localized.opens_a_new_window;
+          } else {
+              anww_label += anww_localized.opens_a_new_window;
+          }
+
+          // Add aria-label to link 
+          $(this).attr("aria-label", anww_label);
+
+          // Position and show link_tooltip on hover
+          $(this).mousemove(function (e) {
+              anww_link_tooltip.css({
+                  top: e.pageY + 10 + 'px',
+                  left: e.pageX + 10 + 'px'
+              });
+          })
+          .hover(function () {
+              anww_link_tooltip.show().html(anww_localized.opens_a_new_window);
+          }, function () {
+              anww_link_tooltip.hide();
+          });
+
+          // Position and show link_tooltip on focus
+          $(this).on({
+              focusin: function () {
+                  var position = $(this).offset();
+                  anww_link_tooltip.css({
+                      top: position.top + $(this).outerHeight() + 'px',
+                      left: position.left + 'px'
+                  });
+
+                  anww_link_tooltip.show().html(anww_localized.opens_a_new_window);
+              },
+              focusout: function () {
+                  anww_link_tooltip.hide();
+              }
+          });
       });
+  }
 
-      // position and show link_tooltip on focus
-      $(this).on({
-        focusin: function () {
-          var position = $(this).offset();
-          anww_link_tooltip.css({
-                top: position.top + $(this).outerHeight() + 'px',
-                left: position.left + 'px'
-            });
-
-          anww_link_tooltip.show().html(anww_localized.opens_a_new_window);
-        },
-        focusout: function () {
-            anww_link_tooltip.hide();
-        }
-      });
-
-    });
-
+  $(window).on('load', function () {
+      initializeTooltip();
+      processLinks();
   });
+
+  // Support for FacetWP: Re-run the processLinks function when FacetWP refreshes the page
+  $(document).on('facetwp-loaded', processLinks);
+
 })(jQuery);
