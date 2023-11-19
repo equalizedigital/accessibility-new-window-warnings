f019d56e40b38c60696f5db7720e4d38ee1be6a4
Steve Jones <steve@stevejonesdev.com>
2023-11-18 23:23:24 -0500

    updated - namespace and class structure

--- accessibility-new-window-warnings.php
+++ accessibility-new-window-warnings.php
@@ -13,76 +13,21 @@
  * @package ANWW
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+use EqualizeDigital\ANWW\Inc\AccessibilityNewWindowWarnings;
+
 define( 'ANWW_VERSION', '1.0.8' );
 define( 'ANWW_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'ANWW_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-if ( ! class_exists( 'ANWW' ) ) {
-	/**
-	 * ANWW Class
-	 * 
-	 * This class sets up actions for enqueuing styles and scripts
-	 * for making links that open in a new window accessible by adding a warning.
-	 */
-	class ANWW {
+/* ***************************** CLASS AUTOLOADING *************************** */
+if ( file_exists( ANWW_PLUGIN_DIR . 'vendor/autoload.php' ) ) {
+	require ANWW_PLUGIN_DIR . 'vendor/autoload.php';
+}
 
-		/**
-		 * Singleton instance of the class.
-		 *
-		 * @var ANWW
-		 */
-		private static $instance;
-
-		/**
-		 * Get the singleton instance of the class.
-		 */
-		public static function get_instance() {
-			if ( null === static::$instance ) {
-				static::$instance = new static();
-			}
-			return static::$instance;
-		}
-		
-		/**
-		 * ANWW constructor.
-		 * Sets up hooks for enqueuing styles and scripts
-		 */
-		public function __construct() {
-			$this->setup_actions();
-		}
-		
-		/**
-		 * Sets up hooks for enqueuing styles and scripts.
-		 * Hooks are added to 'wp_enqueue_scripts'
-		 */
-		public function setup_actions() {
-			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
-		}
-
-		/**
-		 * Enqueues stylesheet for the plugin.
-		 * Stylesheet is located at the plugin's assets directory.
-		 */
-		public function enqueue_styles() {
-			wp_enqueue_style( 'anww', ANWW_PLUGIN_URL . 'assets/css/accessibility-new-window-warnings-min.css', array(), ANWW_VERSION, 'all' );
-		}
-
-		/**
-		 * Enqueues JavaScript file for the plugin.
-		 * JS file is located at the plugin's assets directory.
-		 */
-		public function enqueue_scripts() {
-			wp_enqueue_script( 'anww', ANWW_PLUGIN_URL . 'assets/js/accessibility-new-window-warnings-min.js', array( 'jquery' ), ANWW_VERSION, true );
-
-			// Localize the script with new data.
-			$translation_array = array(
-				'opens_a_new_window' => __( 'opens a new window', 'accessibility-new-window-warnings' ),
-			);
-			wp_localize_script( 'anww', 'anww_localized', $translation_array );
-		}
-	}
-	
-	// instantiate the plugin class.
-	ANWW::get_instance();
+if ( class_exists( 'EqualizeDigital\ANWW\Inc\AccessibilityNewWindowWarnings' ) ) {
+	new AccessibilityNewWindowWarnings();
 }
---
+++ inc/AccessibilityNewWindowWarnings.php
@@ -0,0 +1,56 @@
+<?php 
+/**
+ * Class file for the Accessibility New Window Warnings plugin.
+ * 
+ * @package ANWW
+ */
+
+namespace EqualizeDigital\ANWW\Inc;
+
+/**
+ * AccessibilityNewWindowWarnings Class
+ * 
+ * This class sets up actions for enqueuing styles and scripts
+ * for making links that open in a new window accessible by adding a warning.
+ */
+class AccessibilityNewWindowWarnings {
+
+	/**
+	 * ANWW constructor.
+	 * Sets up hooks for enqueuing styles and scripts
+	 */
+	public function __construct() {
+		$this->setup_actions();
+	}
+
+	/**
+	 * Sets up hooks for enqueuing styles and scripts.
+	 * Hooks are added to 'wp_enqueue_scripts'
+	 */
+	public function setup_actions() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+	}
+
+	/**
+	 * Enqueues stylesheet for the plugin.
+	 * Stylesheet is located at the plugin's assets directory.
+	 */
+	public function enqueue_styles() {
+		wp_enqueue_style( 'anww', ANWW_PLUGIN_URL . 'assets/css/accessibility-new-window-warnings-min.css', array(), ANWW_VERSION, 'all' );
+	}
+
+	/**
+	 * Enqueues JavaScript file for the plugin.
+	 * JS file is located at the plugin's assets directory.
+	 */
+	public function enqueue_scripts() {
+		wp_enqueue_script( 'anww', ANWW_PLUGIN_URL . 'assets/js/accessibility-new-window-warnings-min.js', array( 'jquery' ), ANWW_VERSION, true );
+
+		// Localize the script with new data.
+		$translation_array = array(
+			'opens_a_new_window' => __( 'opens a new window', 'accessibility-new-window-warnings' ),
+		);
+		wp_localize_script( 'anww', 'anww_localized', $translation_array );
+	}
+}
